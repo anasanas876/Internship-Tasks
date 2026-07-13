a052f053b4f13f624aa11900762de9c89a01df1e
anasanas876 <okashaanas986@gmail.com>
2026-07-12 23:14:50 -0700

    Python Core Basics tasks uploaded

---
+++ Task3.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter 1st number"))
+num2=int(input("Enter 1st number"))
+print(f"addition {num1+num2}")
+print(f"subtraction {num1-num2}")
+print(f"multiplication {num1*num2}")
+print(f"division {num1/num2}")
---
+++ Task4.py
@@ -0,0 +1,4 @@
+price=int(input("Enter a price"))
+quantity=int(input("Enter a quantity"))
+total_bill=price * quantity
+print(total_bill)
