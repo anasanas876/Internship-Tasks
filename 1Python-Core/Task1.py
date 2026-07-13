6f7f3c6ab5f886436ae4ab0e650df506ab701c6d
anasanas876 <okashaanas986@gmail.com>
2026-07-13 05:30:05 -0700

    Folder Structure Updated

---
+++ 1Python-Core/Task1.py
@@ -0,0 +1,4 @@
+# Task 1
+age=int(input("Enter your age"))
+name=input("Enter your name")
+print(f"Hi {name}, next year you will be {age+1} years old")
---
+++ 1Python-Core/Task2.py
@@ -0,0 +1,4 @@
+# Task 1
+celcius=int(input("Enter temperature in celcius"))
+farenhite=celcius * 9/5+32
+print(farenhite)
